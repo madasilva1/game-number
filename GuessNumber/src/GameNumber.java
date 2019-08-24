e27e348a3c8be1b9aed94a2ceab3bcb041d428de
marcos <madasilva195@gmail.com>
2019-08-23 23:34:50 -0400

    Finaly random number is reveled at the end of game

--- GuessNumber/src/GameNumber.java
+++ GuessNumber/src/GameNumber.java
@@ -3,30 +3,33 @@ import java.util.Scanner;
 
 public class GameNumber {
 
-    public static void main(String [] args){
-        int randomNumber = (int) (Math.random( ) * 100 ) + 1;
+    public static void main(String [] args) {
+        int randomNumber = (int) (Math.random() * 100) + 1;
         System.out.println(" I have randomly number from 1 to 100");
         System.out.println("Choos one :");
         Scanner scane = new Scanner(System.in);
-        for( int i = 10; i >0 ; i--){
-            System.out.println(" You still have " +  i +  " choices left,choose again: ");
+        for (int i = 10; i > 0; i--) {
+            System.out.println(" You still have " + i + " choices left,choose again: ");
             int guess = scane.nextInt();
             System.out.println(guess);
-            if( randomNumber > guess) {
+            if (randomNumber > guess) {
                 System.out.println(" Larger than that ");
             }
-                if (randomNumber < guess ){
-                    System.out.println(" Smaller than that");
-                }
-                if (randomNumber == guess){
-                    System.out.println("You won !");
-                    break;
-                }
-                if ( randomNumber != guess && i == 1){
-                    System.out.println(" You lost");
-                    break;
-                }
+            if (randomNumber < guess) {
+                System.out.println(" Smaller than that");
             }
+            if (randomNumber == guess) {
+                System.out.println("You won !");
+                break;
+            }
+            if (randomNumber != guess && i == 1) {
+                System.out.println(" You lost");
+                break;
+            }
+
         }
+        System.out.println("Random Number is: " + randomNumber + "thank you");
+    }
+
     }
 
