6fad286489e6255f405c0121b306b2c9cbe5378f
marcos <madasilva195@gmail.com>
2019-08-23 22:15:25 -0400

    andomly has beem add

---
+++ GuessNumber/src/GameNumber.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class GameNumber {
+
+    public static void main(String [] args){
+        int randomNumber = (int) (Math.random( ) * 100 ) + 1;
+        System.out.println(" I have randomly number from 1 to 100");
+        System.out.println("Choos one :");
+        System.out.println(randomNumber);
+    }
+}
