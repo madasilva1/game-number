dffbcca2b0ea1c1c3438b8a205981c08b5e13efb
marcos <madasilva195@gmail.com>
2019-08-23 22:45:22 -0400

    Geting input from user

--- GuessNumber/src/GameNumber.java
+++ GuessNumber/src/GameNumber.java
@@ -7,8 +7,11 @@ public class GameNumber {
         int randomNumber = (int) (Math.random( ) * 100 ) + 1;
         System.out.println(" I have randomly number from 1 to 100");
         System.out.println("Choos one :");
+        Scanner scane = new Scanner(System.in);
         for( int i = 10; i >0 ; i--){
             System.out.println(" You still have " +  i +  " choices left,choose again: ");
+            int guess = scane.nextInt();
+            System.out.println(guess);
         }
     }
 }
